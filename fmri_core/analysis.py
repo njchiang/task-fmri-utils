9951f4ab8194fc5ac562d7d026b7828a440d06e5
Jeff <jeff.njchiang@gmail.com>
2017-08-17 08:38:32 -0700

    start design mat

--- fmri_core/analysis.py
+++ fmri_core/analysis.py
@@ -52,6 +52,100 @@ def sgfilter(logger=None, **sgparams):
 #######################################
 # Analysis
 #######################################
+
+def make_lss_reg():
+    if isinstance(condition_attr, basestring):
+        # must be a list/tuple/array for the logic below
+        condition_attr = [condition_attr]
+
+    e = copy.deepcopy(eorig)  # since we are modifying in place
+    glm_condition_attrs = []
+    for i, con in enumerate(condition_attr):
+        glm_condition_attr = 'regressors_' + str(con)
+        glm_condition_attrs.append(glm_condition_attr)
+        for ei in e:
+            if glm_condition_attr in ei:
+                raise ValueError("Event %s already has %s defined.  Should not "
+                                 "happen.  Choose another name if defined it"
+                                 % (ei, glm_condition_attr))
+            ei[glm_condition_attr] = \
+                'glm_label_' + str(con) + '_' + '+'.join(str(ei[c]) for c in [con])
+    pass
+# TODO : Add design matrix
+# new make_designmat from scratch
+def make_designmat(tr, events, design_kwargs=None, regr_attrs=None):
+    # make glm regressors for all attributes. so loop through condition_attr and add them all...
+    import copy
+    from nipy.modalities.fmri.design_matrix import make_dmtx
+    import numpy as np
+    # Decide/device condition attribute on which GLM will actually be done
+    evvars = events2dict(e)
+    add_paradigm_kwargs = {}
+    if 'amplitude' in evvars:
+        add_paradigm_kwargs['amplitude'] = evvars['amplitude']
+    if design_kwargs is None:
+        design_kwargs = {}
+    if regr_attrs is not None:
+        names = []
+        regrs = []
+        for attr in regr_attrs:
+            regr = ds.sa[attr].value
+            # add rudimentary dimension for easy hstacking later on
+            if regr.ndim < 2:
+                regr = regr[:, np.newaxis]
+            if regr.shape[1] == 1:
+                names.append(attr)
+            else:
+                #  add one per each column of the regressor
+                for i in xrange(regr.shape[1]):
+                    names.append("%s.%d" % (attr, i))
+            regrs.append(regr)
+        regrs = np.hstack(regrs)
+
+        if 'add_regs' in design_kwargs:
+            design_kwargs['add_regs'] = np.hstack((design_kwargs['add_regs'],
+                                                   regrs))
+        else:
+            design_kwargs['add_regs'] = regrs
+        if 'add_reg_names' in design_kwargs:
+            design_kwargs['add_reg_names'].extend(names)
+        else:
+            design_kwargs['add_reg_names'] = names
+
+    x = {}
+    for ci, con in enumerate(condition_attr):
+        # create paradigm
+        if 'duration' in evvars:
+            from nipy.modalities.fmri.experimental_paradigm import BlockParadigm
+            # NiPy considers everything with a duration as a block paradigm
+            paradigm = BlockParadigm(
+                con_id=evvars[glm_condition_attrs[ci]],
+                onset=evvars['onset'],
+                duration=evvars['duration'],
+                **add_paradigm_kwargs)
+        else:
+            from nipy.modalities.fmri.experimental_paradigm \
+                import EventRelatedParadigm
+            paradigm = EventRelatedParadigm(
+                con_id=evvars[glm_condition_attrs[ci]],
+                onset=evvars['onset'],
+                **add_paradigm_kwargs)
+        x[con] = make_dmtx(ds.sa[time_attr].value, paradigm=paradigm, **design_kwargs)
+        for i, reg in enumerate(x[con].names):
+            ds.sa[reg] = x[con].matrix[:, i]
+        if con in ds.sa.keys():
+            ds.sa.pop(con)
+
+        for reg in ds.sa.keys():
+            if str(con)+'0' in reg:
+                ds.sa['glm_label_probe'] = ds.sa.pop(reg)
+
+    # concatenate X... add chunk regressors...
+    # if 'chunks' in ds.sa.keys():
+    #     for i in ds.sa['chunks'].unique:
+    #         ds.sa['glm_label_chunks' + str(i)] = np.array(ds.sa['chunks'].value == i, dtype=np.int)
+    return x, ds
+
 # TODO : Add RSA functionality (needs a .fit)
 def covdiag(x, df=None, shrinkage=None, logger=None):
     """
