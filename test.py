aec4f87e21288e6d3450664fd95c90a13e7d4eaa
njchiang <jeff.njchiang@gmail.com>
2019-08-09 17:21:11 -0700

    add basic import testing

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import os
 
+from fmri_core.analysis import *
+from fmri_core.utils import *
+from fmri_core.vis import *
+
+
+ 
+# for now, just import testing 
+# TODO : more sophisticated tests
 def main():
     assert True, "something's broken"
 
